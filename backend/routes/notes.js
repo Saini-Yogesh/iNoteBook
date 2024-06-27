25fc9c1242a5595d5158f91522a09a9dbca80691
Yogesh Saini <yogesh.saini4002@gmail.com>
2024-06-27 16:04:44 +0530

    Updating an existing Note

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -54,4 +54,40 @@ router.post(
   }
 );
 
+// ROUTE :3
+// Update Notes using put:  "/updateNote" login required
+router.put("/updateNote/:id", fetchUser, async (req, res) => {
+  try {
+    const { tittle, description, tag } = req.body;
+    // create a newNote object
+    const newNote = {};
+    if (tittle) {
+      newNote.tittle = tittle;
+    }
+    if (description) {
+      newNote.description = description;
+    }
+    if (tag) {
+      newNote.tag = tag;
+    }
+    // find the node to be updated and upgate it
+    let note = await Notes.findById(req.params.id);
+    if (!note) {
+      return res.status(404).send("Not Found");
+    }
+    if (note.user.toString() !== req.user.id) {
+      return res.status(401).send("Not Allowed");
+    }
+    note = await Notes.findByIdAndUpdate(
+      req.params.id,
+      { $set: newNote },
+      { new: true }
+    );
+    res.json(note);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
 module.exports = router;
